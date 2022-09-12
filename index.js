195329ea63eb8579997b18eeb11041b1e779b070
Jason Liao <liao.jason2@gmail.com>
2022-09-12 21:57:04 +0800

    Add bot

---
+++ index.js
@@ -0,0 +1,30 @@
+var TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config(".env"); 
+var token = process.env.CHANNEL_TOKEN;
+
+var bot = new TelegramBot(token, { polling: true });
+
+// /start
+bot.onText(/\/start/, function (msg) {
+  var chatId = msg.chat.id; 
+  var resp = "Hi, 這是 Justin 拉麵 bot！"; 
+  bot.sendMessage(chatId, resp);
+});
+
+// /help
+bot.onText(/\/help/, function (msg) {
+  var chatId = msg.chat.id;
+  var resp = "Justin 請我吃拉麵ㄒㄒ bot\n\n ";
+  var startFunction = "/start"  
+  var pollFunction = "/poll 開啟投票"
+  var resp = "賈斯丁拉麵 bot\n\n" + startFunction + "\n" + pollFunction;
+  bot.sendMessage(chatId, resp);
+});
+
+// /poll
+bot.onText(/\/poll/, function (msg) {
+  var chatId = msg.chat.id;
+  bot.sendPoll(chatId, "今天要吃拉麵嗎", ["要", "不要"], {
+    is_anonymous: false,
+  });
+});
