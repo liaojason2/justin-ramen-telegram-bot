db79f7c84ce26460b94bbb66e8bf6e933fe73e57
Jason Liao <liao.jason2@gmail.com>
2022-09-13 12:44:01 +0800

    Fix bug in location

--- index.js
+++ index.js
@@ -76,6 +76,6 @@ bot.onText(/\/location/, (msg) => {
   const chatId = msg.chat.id;
   const latitude = 25.04406477400013;
   const longitude = 121.53271914128021;
-  bot.sendMessage = '這是麵屋雞金的位置！';
+  bot.sendMessage(chatId, '這是麵屋雞金的位置！');
   bot.sendLocation(chatId, latitude, longitude);
 });
