a07e949fdd72f5d85e396f01ecd562d610a11c1b
simba-fs <simba.fs@gmail.com>
2022-09-13 17:36:21 +0800

    add bot.replyOn

--- index.js
+++ index.js
@@ -1,93 +1,112 @@
-const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-require('dotenv').config('.env');
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+require("dotenv").config(".env");
 
 const token = process.env.CHANNEL_TOKEN;
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
-// /start
-bot.onText(/\/start/, function(msg) {
-  const chatId = msg.chat.id;
-  const replyMessage = 'Hi, 這是 Justin 拉麵 bot！';
-  bot.sendMessage(chatId, replyMessage);
-});
-
-// /help
-bot.onText(/\/help/, function(msg) {
-  const chatId = msg.chat.id;
-  let replyMessage = 'Justin 請我吃拉麵 bot\n';
-  replyMessage += '\n';
-  replyMessage += '/start\n';
-  replyMessage += '/poll 開啟投票\n';
-  replyMessage += '/point Justin 已經累積的點數\n';
-  replyMessage += '/location 麵屋雞金位置\n';
-  bot.sendMessage(chatId, replyMessage);
-});
-
-// /poll
-bot.onText(/\/poll/, function(msg, match) {
-  const chatId = msg.chat.id;
-  const username = msg.from.username;
-  let time = '';
-  if (msg.text.split(' ')[1]) {
-    time = msg.text.split(' ')[1];
-  }
-  const timeRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-  if (username === 'JustinLin099') {
-    if (!time) bot.sendMessage(chatId, 'Justin, 你忘了填時間');
-    else if (!time.match(timeRegex)) {
-      bot.sendMessage(chatId, 'Justin, 這不是時間');
-    } else {
-      message = '今天 ' + time + ' 要吃拉麵嗎';
-      bot.sendPoll(chatId, message, ['要', '不要'], {
-        is_anonymous: false,
-      });
-    }
-  } else {
-    const replyMessage = '你不是 Justin, 你是 ' + username;
-    bot.sendMessage(chatId, replyMessage);
-  }
-});
-
-const getPoint = () => {
-  const data = axios
-      .get('https://ramen.justinlin.tw/data.json')
-      .then(function(response) {
-        return response.data.total;
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  return data;
+bot.replyOn = function (cmd, context) {
+	reg = new RegExp(`^/${cmd}`);
+	const bot = this;
+	bot.onText(reg, async (msg /*, match*/) => {
+		if(context instanceof Function) {
+			let isSent = false;
+			const send = (text) => {
+				if(!isSent) {
+					bot.sendMessage(msg.chat.id, text);
+				}
+			};
+			const result = await context(msg, send);
+			if(!isSent && result) {
+				bot.sendMessage(msg.chat.id, result);
+			}
+		} else {
+			bot.sendMessage(msg.chat.id, context);
+		}
+	});
 };
 
+bot.replyOn("simba", `hello ${1 + 2}`);
+
+// /start
+bot.replyOn("start", "Hi, 這是 Justin 拉麵 bot！");
+
+// /help
+bot.replyOn(
+	"help",
+	`
+Justin 請我吃拉麵 bot
+
+/start - 開始使用
+/poll - 開啟投票
+/point - Justin 已經累積的點數
+/location - 麵屋雞金位置
+/pointrule - 麵屋雞金點數規則
+`
+);
+
+// /poll
+bot.replyOn("poll", (msg, send) => {
+	const username = msg.from.username;
+	let time = "";
+	if (msg.text.split(" ")[1]) {
+		time = msg.text.split(" ")[1];
+	}
+	const timeRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+	if (username === "simbafs") {
+		// if (username === "JustinLin099") {
+		if (!time) {
+			send("Justin, 你忘了填時間");
+		} else if (!time.match(timeRegex)) {
+			send("Justin, 這不是時間");
+		} else {
+			message = `今天 ${time} 要吃拉麵嗎`;
+			bot.sendPoll(msg.chat.id, message, ["要", "不要"], {
+				is_anonymous: false,
+			});
+		}
+	} else {
+		const replyMessage = `你不是 Justin, 你是 ${username}`;
+		bot.sendMessage(chatId, replyMessage);
+	}
+})
+
 // /point
-bot.onText(/\/point$/, async (message) => {
-  const chatId = message.chat.id;
-  const point = await getPoint();
-  replyMessage = 'Justin 目前有 ' + point + ' 點\n';
-  replyMessage += '資料由 @gnehs 提供';
-  bot.sendMessage(chatId, replyMessage);
-});
+const getPoint = () => {
+	return axios
+		.get("https://ramen.justinlin.tw/data.json")
+		.then(function (response) {
+			return response.data.total;
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+};
+
+// $ 是為了避免和 pointrule 衝突
+bot.replyOn("point$", async (msg) => `
+Justin 目前有 ${await getPoint()} 點
+資料由 @gnehs 提供
+`)
 
 // /location
+bot.replyOn("location", (msg, send) => {
+	send("這是麵屋雞金的位置！")
+	const latitude = 25.04406477400013;
+	const longitude = 121.53271914128021;
+	bot.sendLocation(msg.chat.id, latitude, longitude);
+} )
 
-bot.onText(/\/location/, (msg) => {
-  const chatId = msg.chat.id;
-  const latitude = 25.04406477400013;
-  const longitude = 121.53271914128021;
-  bot.sendMessage(chatId, '這是麵屋雞金的位置！');
-  bot.sendLocation(chatId, latitude, longitude);
-});
-
-bot.onText(/\/pointrule$/, (msg) => {
-  const chatId = msg.chat.id;
-  let replyMessage = '1.每消費一碗拉麵可獲得點數一點\n';
-  replyMessage += '2.深夜消費(PM10:00起)點數雙倍送!\n';
-  replyMessage += '(麵屋雞金 新生南路店深夜時段將於近期開放、開放時間請注意麵屋雞金 粉專公告)\n';
-  replyMessage += '3.多人消費可以蓋在同一張點卡上、但是不能多張卡合併點數兌換。\n';
-  replyMessage += '4.消費當下給予點數、後面不補發。\n';
-  replyMessage += '5.累積的點數、可以在五之神製作所重新開幕後、至【五之神製作所】兌換\n';
-  replyMessage += '6.集點卡上兌換品項擇一兌換、以點數最高的品項為準\n';
-  bot.sendMessage(chatId, replyMessage);
-});
+// /pointrule
+bot.replyOn(
+	"pointrule",
+	`
+1.每消費一碗拉麵可獲得點數一點
+2.深夜消費（PM 10:00 起）點數雙倍送！
+（麵屋雞金 新生南路店深夜時段將於近期開放、開放時間請注意麵屋雞金 粉專公告）
+3.多人消費可以蓋在同一張點卡上、但是不能多張卡合併點數兌換
+4.消費當下給予點數、後面不補發
+5.累積的點數，可以在五之神製作所重新開幕後，至【五之神製作所】兌換
+6.集點卡上兌換品項擇一兌換、以點數最高的品項為準
+`
+);
