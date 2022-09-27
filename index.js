b77690fe630a6b3c6306ab46e958667e6b49a31b
yc97463 <hi@imych.one>
2022-09-27 11:10:01 +0800

    fix date variable for creating poll

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ bot.onText(/^\/poll(?:@.*?)?( .*|$)/, function(msg, match) {
     else if (!time.match(timeRegex)) {
       bot.sendMessage(chatId, 'Justin, 這不是時間');
     } else {
-      message = `${today.getMonth()+1}/${today.getDate()} ${time} 要吃拉麵嗎`;
+      const today = new Date();
+      message = `${today.toLocaleDateString('zh-tw', { day: 'numeric', month: 'numeric' })} ${time} 要吃拉麵嗎`;
       const sentPollMessage = bot.sendPoll(chatId, message, ['要', '不要'], {
         is_anonymous: false,
       });
